f3eabe6c02d4b8d6fd47a11865e61d695be641d4
Jason Figueroa <jason.figueroa2@gmail.com>
2018-03-08 19:14:40 -0600

    cleaned up the code base

--- CSGITest/Program.cs
+++ CSGITest/Program.cs
@@ -19,34 +19,16 @@ namespace CSGITest
             Console.WriteLine("Listening...");
         }
 
-        // creating Player
-        //static string playerName = "!logical"; // temporarily hard coded
-        //static string steamId = "76561197994895226"; // temporarily hard coded
-        static string steamId = DbInterface.GetSteamId(); 
-        //GetSteamId(userSteamId);
-        //Player player = new Player(playerName, steamId);
+        static string steamId = DbInterface.GetSteamId();
 
-        // TODO need user_id, the following is a temporary placeholder
-        //static int userId = 1;
-
-        // creating Match
         static Match match = new Match();
-
-        // TODO need match_id, the following is a temporary placeholder 
-        //static int matchId = 1;
-
-        // creating MatchStats
         static MatchStats matchStats = new MatchStats();
-
-        // creating MatchTime, for calculating minutes played per game match
         static MatchTime matchTime = new MatchTime();
 
-        // helper variables for the magic
+        // helper variables to to help control the flow of the application
         static bool stashed = false;
         static int counter = 0;
 
-        // NOTE: application does not account for the player switching teams 
-        // and will only capture the initial team the player joins
         static void OnNewGameState(GameState gs)
         {
             if (gs.Round.Phase == RoundPhase.Live && gs.Player.SteamID == steamId)
@@ -58,45 +40,48 @@ namespace CSGITest
                 if (counter == 1)
                 {
                     matchTime.MatchStart = DateTime.Now;
+
+                    // stored as UTC timestamp
                     match.datetime_start = DateTime.Now.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
                     match.map_name = gs.Map.Name;
                 }
 
                 Console.Clear();
 
-                Console.WriteLine($"counter: {counter}");
-
-                // TODO need better printing
-                Console.WriteLine("MatchStats.JSON");
-                Console.WriteLine(gs.Player.MatchStats.JSON);
-                Console.WriteLine();
-                Console.WriteLine("Map.JSON");
-                Console.WriteLine(gs.Map.JSON);
-                Console.WriteLine();
-                Console.WriteLine("Player.JSON");
-                Console.WriteLine(gs.Player.JSON);
-                Console.WriteLine();
-
+                // NOTE: application does not account for the player switching teams 
+                // and will only post to the db the team the player was on in the final round
                 match.team = gs.Player.Team.ToString();
+
                 Console.WriteLine($"player team: {match.team}");
                 matchStats.kills = gs.Player.MatchStats.Kills;
                 matchStats.assists = gs.Player.MatchStats.Assists;
                 matchStats.deaths = gs.Player.MatchStats.Deaths;
                 matchStats.mvps = gs.Player.MatchStats.MVPs;
                 matchStats.score = gs.Player.MatchStats.Score;
+
+                Console.WriteLine("{");
+                Console.WriteLine($"\tcounter: {counter}");
+                Console.WriteLine($"\tteam: {match.team}");
+                Console.WriteLine($"\tkills: {matchStats.kills}");
+                Console.WriteLine($"\tassists: {matchStats.assists}");
+                Console.WriteLine($"\tdeaths: {matchStats.deaths}");
+                Console.WriteLine($"\tmvps: {matchStats.mvps}");
+                Console.WriteLine($"\tscore: {matchStats.score}");
+                Console.WriteLine("}");
             }
 
             // the stashed flag is needed because without it the program will try 
-            // go save, to the db, the match stats multiple times for one match
+            // go save the match stats multiple times for one match
             if (gs.Map.Phase == MapPhase.GameOver && stashed == false)
             {
+                Console.Clear();
+
                 stashed = true;
                 matchTime.MatchStop = DateTime.Now;
                 matchTime.MatchTotal = matchTime.MatchStop - matchTime.MatchStart;
 
                 match.minutes_played = matchTime.MatchTotal.Minutes;
                 match.round_win_team = gs.Round.WinTeam.ToString();
-                Console.WriteLine($"winning team: {match.round_win_team}");
 
                 // reset counter to help track a round change
                 counter = 0;
